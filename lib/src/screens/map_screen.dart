f65c2c6b6bbb9e21eb8fd57792f1763cb03dc691
Roman Reitschmied <rombox@reitschmied.at>
2023-06-09 21:21:43 +0200

    change map  to openstreetmap

--- lib/src/screens/map_screen.dart
+++ lib/src/screens/map_screen.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:flutter_map_supercluster/flutter_map_supercluster.dart';
+// import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:noefinderlein_flutter/src/settings/settings_controller.dart';
 import 'package:noefinderlein_flutter/src/widgets/map_popup.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../database/database_helper.dart';
 import '../database/tables/location.dart';
@@ -14,6 +16,8 @@ import '../../auth/api.dart';
 
 import '../../l10n/app_localizations_context.dart';
 import '../model/model_marker_with_location.dart';
+
+import 'package:url_launcher/url_launcher.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen(
@@ -44,6 +48,8 @@ class _MapScreenState extends State<MapScreen> {
   late FollowOnLocationUpdate _followOnLocationUpdate;
   late StreamController<double?> _followCurrentLocationStreamController;
   late final SuperclusterImmutableController _superclusterController;
+
+  bool locationAllowed = false;
 
   @override
   void initState() {
@@ -102,6 +108,8 @@ class _MapScreenState extends State<MapScreen> {
                 }
                 return FlutterMap(
                   options: MapOptions(
+                      // nePanBoundary: LatLng(49.5752, 8.8587),
+                      // swPanBoundary: LatLng(45.7823, 17.1608),
                       center: LatLng(48.193557, 15.646935),
                       zoom: 12.0,
                       // plugins: [SuperClusterPlugin()],
@@ -121,18 +129,42 @@ class _MapScreenState extends State<MapScreen> {
                         }
                       }),
                   nonRotatedChildren: [
+                    SimpleAttributionWidget(
+                      // source: const Text('Datenquelle: basemap.at und openstreetmap'),
+                      // onTap: () {
+                      //   Uri webLaunchUri = Uri.parse('https://basemap.at');
+                      //   _launchURL(webLaunchUri);
+                      // },
+                      source: const Text('Map data from openstreetmap'),
+                      onTap: () {
+                        Uri webLaunchUri =
+                            Uri.parse('https://www.openstreetmap.org');
+                        _launchURL(webLaunchUri);
+                      },
+                    ),
                     Positioned(
                       right: 20,
                       bottom: 20,
                       child: FloatingActionButton(
-                        onPressed: () {
-                          // Automatically center the location marker on the map when location updated until user interact with the map.
-                          setState(
-                            () => _followOnLocationUpdate =
-                                FollowOnLocationUpdate.always,
-                          );
-                          // Follow the location marker on the map and zoom the map to level 18.
-                          _followCurrentLocationStreamController.add(13);
+                        onPressed: () async {
+                          if (await Permission.location.request().isGranted) {
+                            if (await Permission
+                                .locationWhenInUse.serviceStatus.isEnabled) {
+                              // Automatically center the location marker on the map when location updated until user interact with the map.
+                              setState(() {
+                                locationAllowed = true;
+                                _followOnLocationUpdate =
+                                    FollowOnLocationUpdate.always;
+                              });
+                              // Follow the location marker on the map and zoom the map to level 18.
+                              _followCurrentLocationStreamController.add(13);
+                            } else {
+                              setState(() => locationAllowed = true);
+                            }
+                          } else {
+                            setState(() => locationAllowed = true);
+                          }
+                          if (await Permission.location.request().isGranted) {}
                         },
                         child: const Icon(
                           Icons.my_location,
@@ -143,18 +175,23 @@ class _MapScreenState extends State<MapScreen> {
                   ],
                   children: [
                     TileLayer(
+                      fallbackUrl:
+                          'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                      subdomains: const ['a', 'b', 'c'],
+                      tileProvider: NetworkTileProvider(),
                       userAgentPackageName: 'at.finderlein.noe',
                       urlTemplate: isDarkMode ? mapUrlDark : mapUrl,
-                      additionalOptions: {
-                        'accessToken': mapBoxAccessToken,
-                        'id': 'mapbox.streets',
-                      },
+                      // errorTileCallback: (tile, error, stackTrace) {},
+                      // errorImage:
+                      //     Image.asset('assets/images/lokalbahn.png').image,
+                      // evictErrorTileStrategy: EvictErrorTileStrategy.dispose,
                     ),
-                    CurrentLocationLayer(
-                      followCurrentLocationStream:
-                          _followCurrentLocationStreamController.stream,
-                      followOnLocationUpdate: _followOnLocationUpdate,
-                    ),
+                    if (locationAllowed)
+                      CurrentLocationLayer(
+                        followCurrentLocationStream:
+                            _followCurrentLocationStreamController.stream,
+                        followOnLocationUpdate: _followOnLocationUpdate,
+                      ),
                     SuperclusterLayer.immutable(
                         controller: _superclusterController,
                         initialMarkers: markers, // Provide your own
@@ -182,3 +219,44 @@ class _MapScreenState extends State<MapScreen> {
         ));
   }
 }
+
+void _launchURL(url) async => await canLaunchUrl(url)
+    ? await launchUrl(url, mode: LaunchMode.externalApplication)
+    : throw 'Could not launch $url';
+
+// Future<Position> _determinePosition() async {
+//   bool serviceEnabled;
+//   LocationPermission permission;
+
+//   // Test if location services are enabled.
+//   serviceEnabled = await Geolocator.isLocationServiceEnabled();
+//   if (!serviceEnabled) {
+//     // Location services are not enabled don't continue
+//     // accessing the position and request users of the
+//     // App to enable the location services.
+//     return Future.error('Location services are disabled.');
+//   }
+
+//   permission = await Geolocator.checkPermission();
+//   if (permission == LocationPermission.denied) {
+//     permission = await Geolocator.requestPermission();
+//     if (permission == LocationPermission.denied) {
+//       // Permissions are denied, next time you could try
+//       // requesting permissions again (this is also where
+//       // Android's shouldShowRequestPermissionRationale
+//       // returned true. According to Android guidelines
+//       // your App should show an explanatory UI now.
+//       return Future.error('Location permissions are denied');
+//     }
+//   }
+
+//   if (permission == LocationPermission.deniedForever) {
+//     // Permissions are denied forever, handle appropriately.
+//     return Future.error(
+//         'Location permissions are permanently denied, we cannot request permissions.');
+//   }
+
+//   // When we reach here, permissions are granted and we can
+//   // continue accessing the position of the device.
+//   return await Geolocator.getCurrentPosition();
+// }
